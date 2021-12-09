d965b77801ab1e8e930fbb81eef491ac88c6579e
Ed <edouardfreeman@gmail.com>
2021-12-09 17:15:23 +0000

    getlettergrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            int count = 0;
+
+            //int betterStudents = Students.Count(item => item.AverageGrade > averageGrade);
+
+
+            foreach(Student theStudent in Students.OrderByDescending(item => item.AverageGrade))
+            {
+                if (theStudent.AverageGrade < averageGrade)
+                    break;
+                count++;
+
+            }
+            double proportion = (double)count / Students.Count;
+            if (proportion <= 0.2)
+                return 'A';
+            else if (proportion <= 0.4)
+                return 'B';
+            else if (proportion <= 0.6)
+                return 'C';
+            else if (proportion <= 0.8)
+                return 'D';
+            
+            return 'F';
+        }
     }
 }
