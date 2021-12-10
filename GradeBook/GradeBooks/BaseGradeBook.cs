8e1d34172f1b65407765e7693080f5a627db7b63
Ed <edouardfreeman@gmail.com>
2021-12-10 10:25:06 +0000

    end

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,11 +16,14 @@ namespace GradeBook.GradeBooks
 
         public GradeBookType Type { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
+
+        public bool IsWeighted { get; set; }
 
         public void AddStudent(Student student)
         {
@@ -108,18 +111,21 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int extra = 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                extra = 1;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + extra;
                 case 'B':
-                    return 3;
+                    return 3 + extra;
                 case 'C':
-                    return 2;
+                    return 2 + extra;
                 case 'D':
-                    return 1;
+                    return 1 + extra;
                 case 'F':
-                    return 0;
+                    return 0 + extra;
             }
             return 0;
         }
